21fc69a628ebcd9e4b5ffc31baf567b1e80e0d8e
Abel <abelberhane@gmail.com>
2017-12-04 08:57:58 -0500

    Flask is installed and working correctly. Successful URL Test.

--- test_webapp.py
+++ test_webapp.py
@@ -1,7 +1,11 @@
-# I need to install Flask, Pip
-# from flask import Flask
-# app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
-# @app.route("/")
-# def hello():
-#     return "Hello World!"
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
